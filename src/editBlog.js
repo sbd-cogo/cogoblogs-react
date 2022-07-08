a2a45d7112d90977ee4104d8040657bc2de7a394
Sugat <Sugat.Dhole@cogoport.com>
2022-07-08 11:28:03 +0530

    edit function

--- src/editBlog.js
+++ src/editBlog.js
@@ -5,38 +5,52 @@ import { useParams } from "react-router";
 function EditBlog(){
 
     const [blog, setBlog] = useState();
+    const [btitle, setBtitle] = useState();
+    const [bdesc, setBdesc] = useState();
+    const [bcontent, setBcontent] = useState();
+    const [btype, setBtype] = useState();
     let params = useParams()
+    
+    
     useEffect(() => {
         fetch('http://localhost:3000/blogs/ids/'+params.id)
         .then((response) => (response.json()))
-        .then((data) => setBlog(data))
+        .then((data) => {
+            setBlog(data); 
+            setBtitle(data.blog_title);
+            setBdesc(data.blog_desc);
+            setBcontent(data.blog_content);
+            setBtype(data.blog_type)})
     }, [])
 
-    document.getElementById('title').value = blog?.blog_title ? blog.blog_title : ""
+
     const makeChanges = () => {
-        var edited_title = document.getElementById('title').value;
-        var edited_desc = document.getElementById('desc').value;
-        var edited_content = document.getElementById('content').value;
-        var edited_blogtype = document.getElementById('blogtype').value;
+        // var edited_title = document.getElementById('title').value;
+        console.log(btitle);
+        // var edited_desc = document.getElementById('desc').value;
+        // var edited_content = document.getElementById('content').value;
+        // var edited_blogtype = document.getElementById('blogtype').value;
 
-        
+        axios.put('http://127.0.0.1:3000/blogs/edit/'+params.id , {
+                blog_title: btitle,
+                blog_desc: bdesc,
+                blog_content: bcontent,
+                blog_type: btype
+            })
+    }
 
-        // fetch('http://127.0.0.1:3000/blogs/edit/'+params.id, method)
-        // axios.put('http://127.0.0.1:3000/blogs/edit/'+params.id , {
-        //         blog_title: edited_title,
-        //         blog_desc: edited_desc,
-        //         blog_content: edited_content,
-        //         blog_type: edited_blogtype
-        //     })
+    const submitHandler = (e) => {
+        e.preventDefault();
+        makeChanges();
     }
 
     return(
         <>
-            <input type="text" id="title" ></input>
-            <textarea id="desc" value={blog?.blog_desc}></textarea>
-            <textarea id="content" value={blog?.blog_content}></textarea>
-            <input type="text" id="blogtype" value={blog?.blog_type} ></input>
-            <button onClick={makeChanges}>Make Changes</button>
+            <input type="text" id="title" value={btitle} onChange={(e) => setBtitle(e.target.value)}></input>
+            <textarea id="desc" value={bdesc} onChange={(e) => setBdesc(e.target.value)} ></textarea>
+            <textarea id="content" value={bcontent} onChange={(e) => setBcontent(e.target.value)}></textarea>
+            <input type="text" id="blogtype" value={btype} onChange={(e) => setBtype(e.target.value)}></input>
+            <button onClick={submitHandler}>Make Changes</button>
         </>
     )
 }
